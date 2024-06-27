bf6c8c86f1ef91329ecca6a5d09cfa1bec0247c7
Raissa Marques de Barros <raissamarquesb@gmail.com>
2024-06-27 14:50:18 -0300

    first commit

---
+++ script03.js
@@ -0,0 +1,45 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if(ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0 ){
+        //window.alert('[ERRO] PREENCHA OS CAMPOS!')
+        //res.innerHTML ='Impossivel contar!'
+    }else{
+
+        res.innerHTML = 'Contando: '
+        // CONVERTER EM NUMERO
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p<=0){
+            window.alert(`Passo invalido! Considerando PASSO 1`)
+            p = 1
+        }
+
+        if(i<f){
+            // CONTAGEM CRESCENTE
+        for(let c=i; c<=f; c+=p){
+            res.innerHTML += `${c} \u{2B50}`
+        }
+        }else{
+            // CONTAGEM DECRESCENTE 
+            for(let c=i; c>=f; c-=p){
+                res.innerHTML += `${c} \u{2B50}`
+            }
+        }
+        res.innerHTML += `\u{1F31F}`
+    }
+}
+
+
+
+    
+   
+
+
+
+
